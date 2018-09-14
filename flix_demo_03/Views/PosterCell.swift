fa48954a7f4b435357d3968acc02d86a3e20c51e
Gordon <gordonsu9450100@gmail.com>
2018-09-13 20:13:31 -0700

    Flicks - Part 2
    assignment 2

---
+++ flix_demo_03/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_demo_03
+//
+//  Created by Gordon on 9/13/18.
+//  Copyright © 2018 Gordon. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
